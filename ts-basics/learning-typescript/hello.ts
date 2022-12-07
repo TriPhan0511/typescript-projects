980ce028a359e608f6637e41ed2d5e1475b94b8b
Tri Phan <tripp@ames.edu.vn>
2022-12-07 10:47:58 +0700

    Updated

--- ts-basics/learning-typescript/hello.ts
+++ ts-basics/learning-typescript/hello.ts
@@ -1,2 +1,15 @@
-const firstName = 'Gergia'
-const nameLength = firstName.length()
+// type Material =  string | number
+
+// interface Painter {
+//   finish(): boolean,
+//   ownMaterials: Material[],
+//   paint(painting: string, materials: Material[]): boolean
+// }
+
+// function paintPaiting(painter: Painter, painting: string): boolean {
+//   painter.finish()
+//   return true
+// }
+
+const artist = 'Augusta Savage'
+console.log({ artist });
